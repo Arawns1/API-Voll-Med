b18001c27add4e10457a248859efa8ea2bb7327e
Gabriel Damico <gabrieldamico22@gmail.com>
2023-11-23 16:29:45 -0300

    feat(paciente): Update paciente implemented

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,9 +3,7 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.Paciente;
-import med.voll.api.dto.DadosCadastroPaciente;
-import med.voll.api.dto.DadosListagemMedicos;
-import med.voll.api.dto.DadosListagemPacientes;
+import med.voll.api.dto.*;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -28,5 +26,12 @@ public class PacienteController {
     @Transactional
     public void savePaciente(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
+    }
+
+    @PutMapping
+    @Transactional
+    public void updatePaciente(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
     }
 }
--- src/main/java/med/voll/api/domain/Paciente.java
+++ src/main/java/med/voll/api/domain/Paciente.java
@@ -2,6 +2,7 @@ package med.voll.api.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import med.voll.api.dto.DadosAtualizacaoPaciente;
 import med.voll.api.dto.DadosCadastroMedico;
 import med.voll.api.dto.DadosCadastroPaciente;
 
@@ -35,4 +36,15 @@ public class Paciente {
         this.endereco = new Endereco(dados.endereco());
     }
 
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
