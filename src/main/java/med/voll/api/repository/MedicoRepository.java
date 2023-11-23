7f5aff15738979d9c669433b7cffadb0eda9b3ae
Gabriel Damico <gabrieldamico22@gmail.com>
2023-11-23 16:20:08 -0300

    feat(Medico): Logical Delete implemented
    - Migration V4 created
    - Method excluir created in Medico model
    - Medico delete endpoint implemented

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,11 +1,15 @@
 package med.voll.api.repository;
 
 import med.voll.api.domain.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, UUID> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
