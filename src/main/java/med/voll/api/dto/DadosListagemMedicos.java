327022d5dbb569452d368bafb79fc05e3dcb3197
Gabriel Damico <gabrieldamico22@gmail.com>
2023-11-23 15:47:43 -0300

    fix(dto): ID implemented in DTOs
    - Id implemented in DTOS DadosListagemPacientes e DadosListagemMedicos

--- src/main/java/med/voll/api/dto/DadosListagemMedicos.java
+++ src/main/java/med/voll/api/dto/DadosListagemMedicos.java
@@ -3,12 +3,15 @@ package med.voll.api.dto;
 import med.voll.api.domain.Especialidade;
 import med.voll.api.domain.Medico;
 
-public record DadosListagemMedicos(String nome,
+import java.util.UUID;
+
+public record DadosListagemMedicos(UUID id,
+                                   String nome,
                                    String email,
                                    String crm,
                                    Especialidade especialidade) {
 
     public DadosListagemMedicos(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
--- src/main/java/med/voll/api/dto/DadosListagemPacientes.java
+++ src/main/java/med/voll/api/dto/DadosListagemPacientes.java
@@ -2,9 +2,11 @@ package med.voll.api.dto;
 
 import med.voll.api.domain.Paciente;
 
-public record DadosListagemPacientes(String nome, String email, String cpf) {
+import java.util.UUID;
+
+public record DadosListagemPacientes(UUID id, String nome, String email, String cpf) {
 
     public DadosListagemPacientes(Paciente paciente){
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
 }
