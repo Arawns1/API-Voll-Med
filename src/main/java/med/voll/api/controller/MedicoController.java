9dc401553ec209263f5967eba3534b78ea3bc283
Gabriel Damico <gabrieldamico22@gmail.com>
2023-11-23 15:09:29 -0300

    feat(Medicos): List endpoint created

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,16 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import med.voll.api.domain.Medico;
 import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.dto.DadosListagemMedicos;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -14,6 +19,11 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
+
+    @GetMapping
+    public List<DadosListagemMedicos> getAllMedicos(){
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    }
     @PostMapping
     @Transactional
     public void saveMedico(@RequestBody @Valid DadosCadastroMedico dados){
